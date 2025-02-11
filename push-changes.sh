055bc23ce0d21ee8da1a930502195e5565d21716
Uku-Kaspar Uustalu <uku-kaspar.uustalu@tufts.edu>
2025-02-11 13:03:59 -0500

    add option to delete specific files from dest

--- push-changes.sh
+++ push-changes.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 home=$(pwd)
-cd $REPO_PATH
+cd "$REPO_PATH" || exit
 git reset --hard
 git pull --all
-git checkout $BRANCH -- || git switch --orphan $BRANCH
-if [ "$CLEAR" == "true" ]; then git rm -rfq *; fi
-rsync -a --exclude=".git" ../$STAGING/ ./
+git checkout "$BRANCH" -- || git switch --orphan "$BRANCH"
+if [ "$CLEAR" == "true" ]
+then
+    git rm -rfq -- *
+else
+    while IFS="" read -r pattern
+        do
+            rm -rf -- "$pattern"
+        done <<< "$(grep '\S' <<< "$RM_LIST")"
+fi
+rsync -a --exclude=".git" ../"$STAGING"/ ./
 git add -A
 git commit -m "$MESSAGE"
-git push -u origin $BRANCH
-cd $home
+git push -u origin "$BRANCH"
+cd "$home" || exit
